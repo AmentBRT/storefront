6a3e2ac6402944b32df3a7a752aa7a9545c65c1c
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-12 23:09:39 +0330

    Update to update a collection object

--- playground/views.py
+++ playground/views.py
@@ -3,6 +3,11 @@ from store.models import Collection
 
 
 def say_hello(request):
-    collection = Collection.objects.create(title='Video Game', featured_product_id=1)
+    collection = Collection(pk=11)
+    collection.title = 'Games'
+    collection.featured_product = None
+    collection.save()
+
+    collection = Collection.objects.filter(pk=11).update(featured_product=None)
 
     return render(request, 'hello.html', {'name': 'Mosh', 'collection': collection})
