7c5063e4f06040da6b0b172cf9538a8b0becec6f
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-12 23:01:03 +0330

    Update to aggregate and annotate objects

--- playground/views.py
+++ playground/views.py
@@ -1,28 +1,22 @@
 from django.shortcuts import render
-from django.db.models  import Q, F
-from store.models import Product, OrderItem, Order
+from django.db.models  import Q, F, Value, Func, ExpressionWrapper, DecimalField
+from django.db.models.functions import Concat
+from django.db.models.aggregates import Count, Min
+from store.models import Product, Customer
 
 
 def say_hello(request):
-    queryset = Product.objects.filter(unit_price__range=(20, 30))
-    queryset = Product.objects.filter(Q(unit_price=20) | Q(inventory=20))
-    queryset = Product.objects.filter(inventory=F('collection__id'))
+    result = Product.objects.aggregate(count=Count('id'), min_price=Min('unit_price'))
 
-    queryset = Product.objects.order_by('-unit_price')
-    product = Product.objects.earliest('unit_price')
-    product = Product.objects.latest('unit_price')
+    result = Product.objects.annotate(is_new=Value(True))
+    result = Customer.objects.annotate(new_id=F('id') + 1)
 
-    queryset = Product.objects.all()[5:10]
+    result = Customer.objects.annotate(full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
+    result = Customer.objects.annotate(full_name=Concat('first_name', Value(' '), 'last_name'))
 
-    queryset = Product.objects.values('id', 'title', 'collection__title')
+    result = Customer.objects.annotate(orders_count=Count('order'))
 
-    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product__id').distinct()).order_by('title')
+    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, DecimalField())
+    result = Product.objects.annotate(discounted_price=discounted_price)
 
-    queryset = Product.objects.only('id', 'title', 'collection__title')
-
-    queryset = Product.objects.select_related('collection').all()
-    queryset = Product.objects.prefetch_related('promotions').all()
-
-    queryset = Order.objects.select_related('customer').order_by('-placed_at')[:5].prefetch_related('orderitem_set__product')
-
-    return render(request, 'hello.html', {'name': 'Mosh', 'orders': queryset})
+    return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
