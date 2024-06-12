8afd3a4da92ea0999d3c89129fcc6fa5e92dd7b1
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-12 23:06:55 +0330

    Update to get generic tags for a product

--- playground/views.py
+++ playground/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
-from django.db.models  import Q, F, Value, Func, ExpressionWrapper, DecimalField
-from django.db.models.functions import Concat
-from django.db.models.aggregates import Count, Min
-from store.models import Product, Customer
+from store.models import Product
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    result = Product.objects.aggregate(count=Count('id'), min_price=Min('unit_price'))
+    queryset = TaggedItem.objects.get_tags_for(Product, 1)
 
-    result = Product.objects.annotate(is_new=Value(True))
-    result = Customer.objects.annotate(new_id=F('id') + 1)
-
-    result = Customer.objects.annotate(full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
-    result = Customer.objects.annotate(full_name=Concat('first_name', Value(' '), 'last_name'))
-
-    result = Customer.objects.annotate(orders_count=Count('order'))
-
-    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, DecimalField())
-    result = Product.objects.annotate(discounted_price=discounted_price)
-
-    return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
+    return render(request, 'hello.html', {'name': 'Mosh', 'tags': list(queryset)})
