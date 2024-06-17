e24a6a8de5a39297730489e157e54035077ae8bc
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-17 04:19:39 +0330

    Update for storefront2, cleaning the view

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
-from django.db import connection
-from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.raw('SELECT id, title FROM store_product')
-
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT id, title, unit_price FROM store_product')
-        # cursor.callproc('get_customers', [1, 2, 'a'])
-
-    return render(request, 'hello.html', {'name': 'Mosh', 'queryset': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Mosh'})
