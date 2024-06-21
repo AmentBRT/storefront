bd95c665bdab2ab34897c63cee288b3c2286fc37
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-21 11:27:34 +0330

    Update ProductSerializer and CollectionSerializer to use ModelSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -6,20 +6,17 @@ from .models import Product, Collection
 
 
 class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
     price_with_tax = serializers.SerializerMethodField('calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        'collection-detail',
-        queryset=Collection.objects.all(),
-        lookup_field='id',
-    )
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
 
     def calculate_tax(self, product: Product):
         return round(product.unit_price * Decimal(1.1), 2)
