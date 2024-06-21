8ba75eeaa9aee0678708ecd64a7e9bb23e05e784
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-21 11:18:26 +0330

    Update product_list view to add request object to context of serializer

--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ from .serializers import ProductSerializer
 @api_view()
 def product_list(request):
     queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True)
+    serializer = ProductSerializer(queryset, many=True, context={'request': request})
     return Response(serializer.data)
 
 
