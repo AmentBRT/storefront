1f682362a5404bd347cd58f3a4c2f3d795cbabee
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-21 11:02:46 +0330

    Update ProductSerializer to use nested CollectionSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -15,7 +15,7 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField('calculate_tax')
-    collection = serializers.StringRelatedField()
+    collection = CollectionSerializer()
 
     def calculate_tax(self, product: Product):
         return round(product.unit_price * Decimal(1.1), 2)
