683efb047cf092bf1551e2b61d4c6349e576a770
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-21 10:53:08 +0330

    Update to add a primary key related field to Collection model in ProductSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,12 +2,17 @@ from rest_framework import serializers
 
 from decimal import Decimal
 
+from .models import Product, Collection
+
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField('calculate_tax')
+    collection = serializers.PrimaryKeyRelatedField(
+        queryset=Collection.objects.all(),
+    )
 
-    def calculate_tax(self, product):
+    def calculate_tax(self, product: Product):
         return round(product.unit_price * Decimal(1.1), 2)
