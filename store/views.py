021b99e03ea8f5e1f9dd666dc4a712ced1d637c2
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-21 09:46:05 +0330

    Update product_detail to use get_object_or_404

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -13,9 +14,6 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, id):
-    try:
-        product = Product.objects.get(id=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    except Product.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    product = get_object_or_404(Product, id=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
