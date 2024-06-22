59b51a1611d3df441665df4b634244b912371ce1
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-22 22:19:11 +0330

    Add CustomerSerializer to use ModelSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from decimal import Decimal
 
-from .models import Product, Collection, Review, Cart, CartItem
+from .models import Product, Collection, Review, Cart, CartItem, Customer
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -96,3 +96,11 @@ class CartSerializer(serializers.ModelSerializer):
 
     def get_total_price(self, cart: Cart):
         return sum([item.product.unit_price * item.quantity for item in cart.items.all()])
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField()
+
+    class Meta:
+        model = Customer
+        fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
