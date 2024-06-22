ae8c7285615f8b35bddafc9672f4aa58f9c26c9c
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-22 15:02:47 +0330

    Add CartItemSerializer and CartItemProductSerializer serializer and update CartSerializer to add items and total_price fields

--- store/serializers.py
+++ store/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from decimal import Decimal
 
-from .models import Product, Collection, Review, Cart
+from .models import Product, Collection, Review, Cart, CartItem
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -34,9 +34,32 @@ class ReviewSerializer(serializers.ModelSerializer):
         return super().create(validated_data)
 
 
+class CartItemProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price']
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = CartItemProductSerializer()
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CartItem
+        fields = ['product', 'quantity', 'total_price']
+
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.product.unit_price * cart_item.quantity
+
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True)
+    total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = Cart
-        fields = ['id']
+        fields = ['id', 'items', 'total_price']
+
+    def get_total_price(self, cart: Cart):
+        return sum([item.product.unit_price * item.quantity for item in cart.items.all()])
+
