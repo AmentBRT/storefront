79bb0ef13e0b09ac963a01efa59f2238ef791592
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-22 18:10:59 +0330

    Add AddCartItemSerializer serializer

--- store/serializers.py
+++ store/serializers.py
@@ -51,6 +51,34 @@ class CartItemSerializer(serializers.ModelSerializer):
     def get_total_price(self, cart_item: CartItem):
         return cart_item.product.unit_price * cart_item.quantity
 
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField()
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product_id', 'quantity']
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(id=value).exists():
+            raise serializers.ValidationError('No Product with the given ID was found.')
+
+        return value
+
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        product_id = self.validated_data['product_id']
+        quantity = self.validated_data['quantity']
+
+        self.instance, created = CartItem.objects.get_or_create(cart_id=cart_id, product_id=product_id, defaults={'quantity': quantity})
+
+        if not created:
+            self.instance.quantity += quantity
+            self.instance.save()
+
+        return self.instance
+
+
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     items = CartItemSerializer(many=True, read_only=True)
@@ -62,4 +90,3 @@ class CartSerializer(serializers.ModelSerializer):
 
     def get_total_price(self, cart: Cart):
         return sum([item.product.unit_price * item.quantity for item in cart.items.all()])
-
