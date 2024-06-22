4374138d62c76c984475c8a976fd2c7b94c8935e
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-22 20:48:48 +0330

    Update Order model to add cancel_oreder permission

--- store/models.py
+++ store/models.py
@@ -80,6 +80,11 @@ class Order(models.Model):
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name='orders')
 
+    class Meta:
+        permissions = [
+            ('cancel_order', 'Can cancle order'),
+        ]
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='orderitems')
