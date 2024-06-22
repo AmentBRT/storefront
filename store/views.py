25f0a8a6755060edeed01d01545f04d054e9758d
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-22 09:58:38 +0330

    Update to use class-based APIView for ProductList and ProductDetail

--- store/views.py
+++ store/views.py
@@ -3,48 +3,52 @@ from django.db.models import Count
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+class ProductList(APIView):
+    def get(self, request):
         queryset = Product.objects.select_related('collection').all()
         serializer = ProductSerializer(queryset, many=True, context={'request': request})
+
         return Response(serializer.data)
 
-    serializer = ProductSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, id):
-    product = get_object_or_404(Product, id=id)
-
-    if request.method == 'GET':
+class ProductDetail(APIView):
+    def get(self, request, id):
+        product = get_object_or_404(Product, id=id)
         serializer = ProductSerializer(product)
+
         return Response(serializer.data)
 
-    if request.method == 'PUT':
+    def put(self, request, id):
+        product = get_object_or_404(Product, id=id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
         return Response(serializer.data)
-
-    if product.orderitems.count() > 0:
-        return Response(
-            {'error': 'Product can not be deleted because it is associated with an order item.'},
-            status=status.HTTP_405_METHOD_NOT_ALLOWED,
-        )
-    product.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def delete(self, request, id):
+        product = get_object_or_404(Product, id=id)
+        if product.orderitems.count() > 0:
+            return Response(
+                {'error': 'Product can not be deleted because it is associated with an order item.'},
+                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            )
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET', 'POST'])
