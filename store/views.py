78778cb43fee2b988da7d8f1bdf8220081a28c69
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-22 10:48:27 +0330

    Update ProductDetail to use RetrieveUpdateDestroyAPIView, also overriding delete method

--- store/views.py
+++ store/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import get_object_or_404
 from django.db.models import Count
 
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import status
 
 from .models import Product, Collection
@@ -16,21 +15,11 @@ class ProductList(ListCreateAPIView):
     serializer_class = ProductSerializer
 
 
-class ProductDetail(APIView):
-    def get(self, request, id):
-        product = get_object_or_404(Product, id=id)
-        serializer = ProductSerializer(product)
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
 
-        return Response(serializer.data)
-
-    def put(self, request, id):
-        product = get_object_or_404(Product, id=id)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data)
-    
     def delete(self, request, id):
         product = get_object_or_404(Product, id=id)
         if product.orderitems.count() > 0:
