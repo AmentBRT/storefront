2b117eff1f86b7037713fe839cd569510d6b637d
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-06-24 22:25:58 +0330

    Update CreateOrderSerializer to send order_created signal

--- store/serializers.py
+++ store/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 from decimal import Decimal
 
 from .models import Product, Collection, Review, Cart, CartItem, Customer, Order, OrderItem
+from .signals import order_created
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -159,6 +160,8 @@ class CreateOrderSerializer(serializers.Serializer):
             OrderItem.objects.bulk_create(order_items)
 
             Cart.objects.get(id=cart_id).delete()
+
+        order_created.send_robust(self.__class__, order=order)
 
         return order
 
